b191cb02545e941b77e2f2e1ab36462cc02f1f31
Sachin Saini <iamsainisachin@gmail.com>
2025-08-29 02:11:07 +0530

    feat: create an abstraction on top of control byte and the control byte container

--- src/control.rs
+++ src/control.rs
@@ -1,38 +1,104 @@
-const EMPTY: u8 = 0x80;
-const DELETED: u8 = 0xFE;
-const SENTINEL: u8 = 0xFF;
+#[repr(transparent)]
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct ControlByte(u8);
 
-#[inline]
-pub fn is_empty(ctrl: u8) -> bool {
-    ctrl == EMPTY
+#[derive(Debug)]
+pub struct ControlBytes {
+    bytes: Box<[ControlByte]>,
+    capacity: usize,
 }
 
-#[inline]
-pub fn is_deleted(ctrl: u8) -> bool {
-    ctrl == DELETED
+impl ControlByte {
+    pub const EMPTY: u8 = 0x80;
+    pub const DELETED: u8 = 0xFE;
+    pub const SENTINEL: u8 = 0xFF;
+
+    pub const EMPTY_B: ControlByte = ControlByte(Self::EMPTY);
+    pub const DELETED_B: ControlByte = ControlByte(Self::DELETED);
+    pub const SENTINEL_B: ControlByte = ControlByte(Self::SENTINEL);
+
+    // the MSB is not set for full control bytes
+    #[inline]
+    pub fn is_full(self) -> bool {
+        self.0 & 0x80 == 0
+    }
+
+    #[inline]
+    pub fn is_empty(self) -> bool {
+        self.0 == Self::EMPTY
+    }
+
+    #[inline]
+    pub fn is_deleted(self) -> bool {
+        self.0 == Self::DELETED
+    }
+
+    #[inline]
+    pub fn tag(self) -> u8 {
+        return self.0 & 0x7F;
+    }
+
+    #[inline]
+    pub fn from_tag(tag: u8) -> Self {
+        ControlByte(tag & 0x7F)
+    }
 }
 
-#[inline]
-pub fn is_full(ctrl: u8) -> bool {
-    ctrl & 0x80 == 0
-}
+impl ControlBytes {
+    pub fn new(capacity: usize) -> Self {
+        let mut bytes: Vec<ControlByte> = vec![ControlByte::EMPTY_B; capacity + 16];
+        bytes[capacity] = ControlByte::SENTINEL_B;
 
-#[inline]
-pub fn ctrl_h2(ctrl: u8) -> u8 {
-    ctrl & 0x7F
-}
+        debug_assert!(bytes.len() == capacity + 16);
+        debug_assert!(bytes[capacity] == ControlByte::DELETED_B);
+        ControlBytes {
+            bytes: bytes.into_boxed_slice(),
+            capacity,
+        }
+    }
 
-#[inline]
-pub fn ctrl_empty() -> u8 {
-    EMPTY
-}
+    #[inline]
+    pub fn capacity(&self) -> usize {
+        self.capacity
+    }
 
-#[inline]
-pub fn ctrl_deleted() -> u8 {
-    DELETED
-}
+    #[inline]
+    pub fn at(&self, idx: usize) -> ControlByte {
+        self.bytes[idx]
+    }
 
-#[inline]
-pub fn ctrl_sentinel() -> u8 {
-    SENTINEL
+    #[inline]
+    fn clone_idx(&self, idx: usize) -> Option<usize> {
+        if idx >= 15 {
+            return None;
+        }
+
+        let clone_idx = self.capacity + idx + 1;
+
+        Some(clone_idx)
+    }
+
+    #[inline]
+    pub fn set_full(&mut self, idx: usize, tag: u8) {
+        self.bytes[idx] = ControlByte::from_tag(tag);
+        if let Some(clone_idx) = self.clone_idx(idx) {
+            self.bytes[clone_idx] = self.bytes[idx];
+        }
+    }
+
+    #[inline]
+    pub fn set_deleted(&mut self, idx: usize) {
+        self.bytes[idx] = ControlByte::DELETED_B;
+        if let Some(clone_idx) = self.clone_idx(idx) {
+            self.bytes[clone_idx] = self.bytes[idx];
+        }
+    }
+
+    #[inline]
+    pub fn set_empty(&mut self, idx: usize) {
+        self.bytes[idx] = ControlByte::EMPTY_B;
+        if let Some(clone_idx) = self.clone_idx(idx) {
+            self.bytes[clone_idx] = self.bytes[idx];
+        }
+    }
 }
